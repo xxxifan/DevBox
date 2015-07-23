ae1abd6155071e5f03156730854cd7ad63639bbb
xxxifan <445947962@qq.com>
2015-07-24 00:11:43 +0800

    add fieldchecker

---
+++ library/src/main/java/com/xxxifan/devbox/library/helpers/FieldChecker.java
@@ -0,0 +1,10 @@
+package com.xxxifan.devbox.library.helpers;
+
+import android.widget.EditText;
+
+/**
+ * Created by xifan on 15-7-24.
+ */
+public interface FieldChecker {
+    boolean checkField(EditText... editTexts);
+}
